e4ee573691546df73b4475fa476955cb1f87ce3b
tatoontoon <iamtoonprapada@gmail.com>
2026-02-06 15:10:04 +0700

    edit cell03_ex03

--- cell03/ex03/todo.js
+++ cell03/ex03/todo.js
@@ -1,53 +1,64 @@
-$(document).ready(function () {
-    loadTasks();
+window.onload = function () {
+    loadTasks(); 
+};
 
-    $('#new-task-btn').on('click', function () {
-        let task = prompt('Enter new task:');
-        if (task) {
-            addTask(task);
-            saveTasks();
+document.getElementById('new-task-btn').addEventListener('click', function () {
+    let task = prompt('Enter new task:');
+    if (task) {
+        addTask(task); 
+        saveTasks();   
+    }
+});
+
+function addTask(task) {
+    const taskDiv = document.createElement('div');
+    taskDiv.textContent = task;
+
+
+    taskDiv.addEventListener('click', function () {
+        if (confirm('Do you want to remove this task?')) {
+            taskDiv.remove();
+            saveTasks(); 
         }
     });
 
-    function addTask(task) {
-        const $taskDiv = $('<div></div>').text(task);
 
-        $taskDiv.on('click', function () {
-            if (confirm('Do you want to remove this task?')) {
-                $taskDiv.remove();
-                saveTasks();
-            }
-        });
+    document.getElementById('ft_list').prepend(taskDiv);
+}
 
-        $('#ft_list').prepend($taskDiv);
+function saveTasks() {
+    const tasks = [];
+    const taskList = document.getElementById('ft_list');
+
+
+    for (let i = 0; i < taskList.children.length; i++) {
+        const taskDiv = taskList.children[i];
+        tasks.push(taskDiv.textContent);
     }
 
-    function saveTasks() {
-        const tasks = [];
-        $('#ft_list').children().each(function () {
-            tasks.push($(this).text());
-        });
 
-        document.cookie = `tasks=${JSON.stringify(tasks)}; path=/; expires=Fri, 31 Dec 9999 23:59:59 GMT`;
-    }
+    document.cookie = `tasks=${JSON.stringify(tasks)}; path=/; expires=Fri, 31 Dec 9999 23:59:59 GMT`;
+}
 
-    function loadTasks() { 
-        const cookies = document.cookie.split('; ');
-        const taskCookie = cookies.find(row => row.startsWith('tasks='));
-        
-        if (taskCookie) {
-            const tasks = JSON.parse(taskCookie.split('=')[1]);
-            $.each(tasks, function(index, task) {
-                const $taskDiv = $('<div></div>').text(task);
-                $taskDiv.on('click', function () {
-                    if (confirm('Do you want to remove this task?')) {
-                        $taskDiv.remove();
-                        saveTasks();  
-                    }
-                });
-                $('#ft_list').append($taskDiv);
+function loadTasks() {
+    const cookies = document.cookie.split(' ');
+    console.log(cookies)
+    const taskCookie = cookies.find(row => row.startsWith('tasks='));
+    if (taskCookie) {
+        const tasks = JSON.parse(taskCookie.split('=')[1]);
+
+        tasks.forEach(task => {
+            const taskDiv = document.createElement('div');
+            taskDiv.textContent = task;
+
+            taskDiv.addEventListener('click', function () {
+                if (confirm('Do you want to remove this task?')) {
+                    taskDiv.remove();
+                    saveTasks(); 
+                }
             });
-        }
+
+            document.getElementById('ft_list').append(taskDiv);
+        });
     }
-    
-});
+}
